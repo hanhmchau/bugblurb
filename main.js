0be6cdd3e8fad83d4b3726bff1f346ef315f11ff
Chau Minh <hanhchaumn@gmail.com>
2020-06-19 20:42:08 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+class ImagePopoutWithBlurb {    
+    static process(imagePopout, html, data) {
+        const blurb = this._getBlurbIfExists(imagePopout);
+        if (blurb) {
+            const lightboxImage = $(html).find('.lightbox-image')[0];
+            if (lightboxImage) {
+                const blurbContainer = document.createElement('div');
+                $(blurbContainer).addClass('blurb-container').html(blurb);
+                $(lightboxImage).after(blurbContainer);
+            }
+        }
+    }
+
+    /** @private */
+    static _getBlurbIfExists(imagePopout) {
+        try {
+            return imagePopout._related.data.data.details.biography.value;
+        } catch (e) {
+            return;
+        }
+    }
+}
+
+Hooks.on("renderImagePopout", ImagePopoutWithBlurb.process.bind(ImagePopoutWithBlurb));
